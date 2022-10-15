85371d97f59ef9aa2753d5b3f4a1ba2ce934e7a7
Loctp <loctp2003@gmail.com>
2022-10-15 11:53:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import TinderCard from 'react-tinder-card';
+import { useState } from 'react';
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: './img/ahihi.png'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: './img/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png'
+  },
+  {
+    name: 'Monica Hall',
+    url: './img/ahihi.png'
+  },
+  {
+    name: 'Jared Dunn',
+    url: './img/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: './img/ahihi.png'
+  }
+]
 
 function App() {
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='cardContainer'>
+        {characters.map((character) =>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+      </div>
+      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
     </div>
   );
 }
